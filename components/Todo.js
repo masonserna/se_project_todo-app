2d8359569af866fbce7abd0d61874e7f2891fbde
Mason Serna <maserna23@gmail.com>
2024-10-13 22:53:34 -0500

    Add methods to Todo class

--- components/Todo.js
+++ components/Todo.js
@@ -1,18 +1,44 @@
 class Todo {
   constructor(data, selector) {
-    console.log(data);
-    console.log(selector);
+    this._data = data;
+    this._templateElement = document.querySelector(selector);
+  }
+
+  _setEventListeners() {
+    this._todoDeleteBtn.addEventListener("click", () => {
+      //   this._todoElement.remove();
+      //   console.log(this._todoElement.remove());
+    });
+
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+      console.log(this._data.completed);
+    });
+  }
+
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
   }
 
   getView() {
-    const todoElement = todoTemplate.content
+    this._todoElement = this._templateElement.content
       .querySelector(".todo")
       .cloneNode(true);
-    const todoNameEl = todoElement.querySelector(".todo__name");
-    const todoCheckboxEl = todoElement.querySelector(".todo__completed");
-    const todoLabel = todoElement.querySelector(".todo__label");
-    const todoDate = todoElement.querySelector(".todo__date");
-    const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
+
+    this._todoNameEl = this._todoElement.querySelector(".todo__name");
+    this._todoDate = this._todoElement.querySelector(".todo__date");
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    this._todoNameEl.textContent = this._data.name;
+
+    this._generateCheckboxEl();
+    this._setEventListeners();
+
+    return this._todoElement;
   }
 }
 
